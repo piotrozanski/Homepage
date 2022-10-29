dda88dcf2cc275a40c61ec5ae6ef8058c7c8ae9a
Piotr Rozanski <piter05@o2.pl>
2022-10-29 11:27:23 +0200

    add function in JS

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
-console.log("Siema");
+{
+const welcome = () => {
+    console.log("Hello World");
+}
+
+const toggleBackground = () => {
+    body.classList.toggle("dark");
+    section__aboutMe.classList.toggle("orange");
+    section__interestedTitle.classList.toggle("orange");
+    navigation__link.forEach(element => {
+    element.classList.toggle("orange")
+    })
+
+    themeName.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
+};
+
+const removeText = () => {
+    section__interestedTitleElement.remove();
+    section__aboutMeElement.remove();
+};
+
+welcome ();
 
 let section__buttonElement = document.querySelector(".section__button");
 let section__aboutMeElement = document.querySelector(".section__aboutMe");
 let section__interestedTitleElement = document.querySelector(".section__interestedTitle");
 
-section__buttonElement.addEventListener("click", () => {
-    section__interestedTitleElement.remove();
-    section__aboutMeElement.remove();
-});
+section__buttonElement.addEventListener("click", removeText);
 
 let button = document.querySelector(".button");
 let body = document.querySelector(".body");
@@ -16,13 +34,5 @@ let section__aboutMe = document.querySelector(".section__aboutMe");
 let section__interestedTitle = document.querySelector(".section__interestedTitle");
 let navigation__link = document.querySelectorAll(".navigation__link");
 
-button.addEventListener("click", () => {
-    body.classList.toggle("dark");
-    section__aboutMe.classList.toggle("orange");
-    section__interestedTitle.classList.toggle("orange");
-    navigation__link.forEach(element => {
-        element.classList.toggle("orange")
-    })
-   
-    themeName.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
-});
+button.addEventListener("click", toggleBackground);
+}
